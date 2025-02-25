b227ab4eeb64dd3d408de3e2a406ef0c49eaed9d
anish102 <anishkarki695@gmail.com>
2025-02-25 23:36:27 +0545

    First react components

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
