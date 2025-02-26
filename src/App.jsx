5e46ea85c8db1589cfa51ac7ed99bd73e51a1c6b
anish102 <anishkarki695@gmail.com>
2025-02-26 17:56:24 +0545

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Card from "./Card";
+import Student from "./Student";
 
 function App() {
   return (
     <>
       <Card />
       <Card />
-      <Card />
-      <Card />
+      <Student name="Tom" age={25} isStudent={true} />
+      <Student name="Sam" age={30} isStudent={false} />
+      <Student name="Jack" age={19} isStudent={true} />
     </>
   );
 }
