64b90b0ea075da9f33819c35cdecca61616d6f34
anish102 <anishkarki695@gmail.com>
2025-02-26 17:07:01 +0545

    Card component

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card">
+            <img className="card-image" src="https://dad.neocities.org/resources/dog7.jpg" alt="image"></img>
+            <h2 className="card-title">Hello World</h2>
+            <p className="card-text">I am learning react js.</p>
+        </div>
+    );
+}
+
+export default Card
