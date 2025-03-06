81154ff0afabaf03e6c2f014644c3870cab6b4ba
anish102 <anishkarki695@gmail.com>
2025-03-06 22:09:32 +0545

    Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import UserGreeting from "./Condition";
+import Card from "./Card";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
+      <Header />
+      <Card />
       <UserGreeting isLoggedIn={true} userName="Sam" />
       <UserGreeting isLoggedIn={false} userName="Jack" />
+      <Footer />
     </>
   );
 }
